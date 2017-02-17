d69fad994121d6944f159c373585d5c68ac4a27b
chclause <chclause@lab1-6.eng.utah.edu>
2017-02-17 15:57:42 -0700

    Added comments and fixed indentations

--- realAnalyzer.cpp
+++ realAnalyzer.cpp
@@ -41,31 +41,38 @@ int main(int argc, char *argv[])
   // maps unstocked items by name to quantity
   map<std::string,int>unstocks;
 
-  // 
+  // maps food names to quantity to help with most popular items
   map<std::string,int>how_many_stocks;
+
+  // maps food names to product shelflife
   map<std::string, int> foodlifes;
 
+  // used to help with popular items
   vector<int> popular;
   
-
-
+  // Read the file line by line
   while (std::getline(in,line))
-
     {
+      // Temp variables
       std::string foodname;
       int shelflife;
       std::string uid;
       std::string word;
-      istringstream iss(line);
- 
+      
+      istringstream iss(line); 
       iss >> word;
-        /* do stuff with word */
+      
+      /* Ugly but works well */
+
+      /* Logic for when a food item must be parsed, based on the 
+	 structure of the file, malformed input will break this */
       if(word.compare("FoodItem")==0)
 	{
 	  iss >> word; 	iss >> word; 	iss >> word; 	iss >> word;
 	  uid=word;
 
 	  iss >> word; 	iss >> word; 	iss >> word; 
+	  
 	  shelflife=atoi(word.c_str());        
 	  iss >> word; 
 	
@@ -74,10 +81,14 @@ int main(int argc, char *argv[])
 	      foodname+=word+=" ";
 	    }
         
+	  /* Store the new items shelflife, name, and initialize it on the 
+	     leaderboard for popular products */
 	  foodlifes[uid]=shelflife;
 	  foodnames[uid]=foodname;
           leaderboard[uid]=0;
 	}
+
+      /* Logic for when a warehouse must be parsed */
       if(word.compare("Warehouse")==0)
 	{
 	  iss >> word; 	iss >> word;
@@ -85,6 +96,8 @@ int main(int argc, char *argv[])
 	  warehouses[word] = new cs3505::warehouse(foodlifes);
 	}
 
+      /* Begin the actual recieve and send logic, first initialize
+	 all the warehouses to be on the correct start date */
       if(word.compare("Start")==0)
 	{
 	   iss >> word; 	iss >> word;
@@ -96,6 +109,8 @@ int main(int argc, char *argv[])
 	     }	   
 	}
       
+      /* Logic for recieving a shipment, storing the food in a warehouse, 
+	 which holds a map of food ids to a deque structure of shipments */
       if (word.compare("Receive:") == 0)
 	{
 	  iss >> word;
@@ -108,6 +123,8 @@ int main(int argc, char *argv[])
 	
 	}
       
+      /* Logic for requested items, removing the items from the warehouse
+         designated */
       if (word.compare("Request:") == 0)
 	{
 	  iss >> word;
@@ -124,6 +141,7 @@ int main(int argc, char *argv[])
 	 
 	}
 
+      /* Change the date in all the warehouses and remove expired food */
       if(word.compare("Next") == 0)
 	{
 	  startdate.goto_tomorrow();
@@ -138,18 +156,25 @@ int main(int argc, char *argv[])
     
     }
     
-  cout<<"report by Linxi and Charlie"<<endl<<endl;
-   cout<<"Unstocked Products:"<<endl;
-   for (std::map<std::string, string>::iterator i = foodnames.begin(); i != foodnames.end(); ++i)
-	     {
-	     
-	       for (std::map<std::string, cs3505::warehouse*>::iterator k = warehouses.begin(); k != warehouses.end(); ++k)
-		 {
-		   int quantity= k->second->get_quantity(i->first);
-		   unstocks[i->first]+=quantity;
-		 }	   
-	     }
+  // Outputting the report and a little bit more logic
+  cout<<"Report by Linxi Li and Charles Clausen"<<endl<<endl;
   
+  cout<<"Unstocked Products:"<<endl;
+
+  /* Iterate the foodnames and warehouses and count the quantity,
+     store the results of this into a map */
+  for (std::map<std::string, string>::iterator i = foodnames.begin(); i != foodnames.end(); ++i)
+    {
+	     
+      for (std::map<std::string, cs3505::warehouse*>::iterator k = warehouses.begin(); k != warehouses.end(); ++k)
+	{
+	  int quantity= k->second->get_quantity(i->first);
+	  unstocks[i->first]+=quantity;
+	 }	   
+     }
+  
+  /* Iterate through the map we just made and print out any item that has 0 product
+     in any warehouse */
    for (std::map<std::string, int>::iterator i = unstocks.begin(); i != unstocks.end(); ++i)
      {
 
@@ -157,22 +182,25 @@ int main(int argc, char *argv[])
 	 {
 	   cout<<i->first<<" "<<foodnames[i->first]<<endl;
 	 }
-
      }	 
+   
    cout<<endl;
+
+   /* Loop through the foodnames and warehouse and count the stock of each */
    cout<<"Well-Stocked Products:"<<endl;
- for (std::map<std::string, string>::iterator i = foodnames.begin(); i != foodnames.end(); ++i)
-	     {
+   for (std::map<std::string, string>::iterator i = foodnames.begin(); i != foodnames.end(); ++i)
+     {
 	     
-	       for (std::map<std::string, cs3505::warehouse*>::iterator k = warehouses.begin(); k != warehouses.end(); ++k)
-		 {
-		   if(k->second->get_quantity(i->first)>0)
-		     {
-		       how_many_stocks[i->first]+=1;
-		     }
-		 }	   
+       for (std::map<std::string, cs3505::warehouse*>::iterator k = warehouses.begin(); k != warehouses.end(); ++k)
+	 {
+	   if(k->second->get_quantity(i->first)>0)
+	     {
+	       how_many_stocks[i->first]+=1;
 	     }
+	 }	   
+      }
   
+   /* Iterate through the map we just made and print the top 3 */
    for (std::map<std::string, int>::iterator i =  how_many_stocks.begin(); i !=  how_many_stocks.end(); ++i)
      {
     
@@ -182,17 +210,19 @@ int main(int argc, char *argv[])
 	 }
 
      }	 
+   
    cout<<endl;
 
-
+   /* Iterate over the leaderboard map and store all items in a vector */
    cout<<"Most Popular Products:"<<endl;
    for (std::map<std::string, int>::iterator it = leaderboard.begin(); it != leaderboard.end(); it++)
-       {
-       popular.push_back(it->second);
-     
-       }
+     {
+       popular.push_back(it->second);      
+     }
+   /* Sort the aforementioned vector */ 
    std::sort(popular.begin(), popular.end());
    std::reverse(popular.begin(), popular.end());
+   /* Print em out */
    for (int i=0; i<popular.size() && i<3; i++)
      { 
        for (std::map<std::string, int>::iterator it = leaderboard.begin(); it != leaderboard.end(); it++)
@@ -206,8 +236,9 @@ int main(int argc, char *argv[])
 
      }
    cout <<endl;
-    
+   
+   // End program
     return 0;
-    }
+}
 
 
